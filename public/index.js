ad09c3285a22e0491a294dc7ca39906a5ad26574
Jennifer Devine <jennifermdevine88@gmail.com>
2023-05-20 11:49:25 -0700

    did activity - open .html pathways via localhost

--- public/index.js
+++ public/index.js
@@ -1,9 +1,21 @@
 async function main() {
 
     let response = await fetch('http://localhost:3001/listBooks')
+    
     let books = await response.json()
-
     books.forEach(renderBook)
+
+    fetch ('http://localhost:3001/updateBook', {
+        method: "PATCH",
+        headers: {
+            'Content-Type': 'application/json'
+        }, 
+        body:
+            JSON.stringify({
+                "id": 3,
+                "title": "The Legends of Arathrae",
+            })
+    })
 }
 
 function renderBook(book) {
